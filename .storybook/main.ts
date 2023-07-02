1f0c90ec6ca62573d28bebe48fd9657a6cf69c5c
Usman Abdur Rehman <saidskit@outlook.com>
2023-07-02 23:12:17 +0500

    edited main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,11 +2,11 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-highlight", // some addon messes with highlight addon so using it only
-    // "@storybook/addon-links",
-    // "@storybook/addon-essentials",
-    // "@storybook/addon-interactions",
-    // "@storybook/addon-a11y",
+    "@storybook/addon-highlight", // some addon messes with highlight addon so should use it alone
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/react-vite",
