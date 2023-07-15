2f08baeff6b9b1b47228c8669a4e90d688d2b6a8
Usman Abdur Rehman <saidskit@outlook.com>
2023-07-15 20:18:44 +0500

    Storybook Designs Addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,11 +2,13 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-highlight", // some addon messes with highlight addon so should use it alone
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
+    // "@storybook/addon-highlight", // some addon messes with highlight addon so should use it alone
+    // "@storybook/addon-links",
+    // "@storybook/addon-essentials",
+    // "@storybook/addon-interactions",
+    // "@storybook/addon-a11y",
+    "@storybook/addon-designs",
+    // "storybook-addon-designs"
   ],
   framework: {
     name: "@storybook/react-vite",
---
+++ src/stories/Designs/Button.stories.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@chakra-ui/react";
+export default {
+  component: Button,
+};
+
+/*
+Figma: https://www.figma.com/file/LKQ4FJ4bTnCSjedbRpk931/Sample-File
+Link: https://www.figma.com/file/Klm6pxIZSaJFiOMX5FpTul9F/storybook-addon-designs-sample
+Image: https://material.angular.io/assets/screenshots/button.scene.png
+IFrame: https://chakra-ui.com/docs/components/button/usage
+*/
+
+export const ButtonBase = {
+  args: {
+    colorScheme: "whatsapp",
+    children: "Sign up",
+  },
+  parameters: {
+    design: [
+      {
+        type: "figma",
+        url: "https://www.figma.com/file/LKQ4FJ4bTnCSjedbRpk931/Sample-File",
+      },
+      {
+        type: "link",
+        url: "https://www.figma.com/file/Klm6pxIZSaJFiOMX5FpTul9F/storybook-addon-designs-sample",
+        label: "Button Design",
+      },
+      {
+        type: "image",
+        url: "https://material.angular.io/assets/screenshots/button.scene.png",
+      },
+      {
+        type: "iframe",
+        url: "https://chakra-ui.com/docs/components/button/usage",
+      },
+    ],
+  },
+};
